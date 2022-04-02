262e84053f785f3abc7471900b6c17602bd31259
mmmmm1233212344 <abhaymilan8@gmail.com>
2022-04-02 14:30:29 +0530

    Second Commit

---
+++ src/components/App.js
@@ -0,0 +1,70 @@
+import React,{useState} from 'react'
+import RecipeList from './RecipeList'
+import '../css/app.css'
+import {v4 as uuidv4} from 'uuid';
+function App(){function handleRecipeAdd(){
+    const newRecipe={
+        id:uuidv4(),
+        name:'food',
+        servings:4,
+        cookTime:'0:30',
+        instructions:"instructions",
+        ingredients:[{
+            id:uuidv4(),
+            name:'Name',
+            amount:'1 Tbs'
+        }]
+        
+    }
+    setRecipes([...recipes,newRecipe])
+}
+function handleRecipeDelete(id){
+setRecipes(recipes.filter(recipe=> recipe.id !== id))
+}
+    
+    const[recipes,setRecipes]=useState(sampleRecipes)
+    
+
+    return(
+        <RecipeList recipes={recipes}
+        handleRecipeAdd={handleRecipeAdd}
+        handleRecipeDelete={handleRecipeDelete}/>
+
+    )
+}
+
+const sampleRecipes=[
+    {
+        id:1,
+        name:'Plain Chicken',
+        servings:3,
+        cookTime:'1:45',
+        instructions:"1. Put salt on chicken \n2. Put chicken in oven \n3. Eat chicken",
+        ingredients:[{
+                id:1,
+                  name:'Chicken',
+                  amount:'2 Pounds'
+        },{
+            id:2,
+                  name:'Salt',
+                  amount:'1 Tbs'
+        }]
+},{
+    id:2,
+    name:'Plain Pork',
+    servings:5,
+    cookTime:'0:45,',
+    instructions:"1. Put paprika on pork\n2. Put pork in oven \n3. Eat pork",
+    ingredients:[{
+        id:1,
+          name:'Pork',
+          amount:'3 Pounds'
+},{
+    id:2,
+          name:'Paprika',
+          amount:' 2 Tbs'
+}]
+}
+]
+
+export default App;
---
+++ src/components/Recipe.js
@@ -0,0 +1,39 @@
+import React from "react";
+import IngredientList from "./IngredientList";
+
+export default function Recipe(props) {
+  const { id,name, cookTime, Servings, instructions, ingredients,handleRecipeDelete } = props;
+  return (
+    <div className="recipe">
+      <div className="recipe__header">
+        <h3 className="recipe__title">{name}</h3>
+        <div>
+          <button className="btn btn--primary mr-1">Edit</button>
+          <button className="btn btn--danger"
+          onClick={()=> handleRecipeDelete(id)}
+          >
+            Delete
+          </button>
+        </div>
+      </div>
+      <div className="recipe__row">
+        <span className="recipe__label">Cook Time:</span>
+        <span className="recipe__value">{cookTime} </span>
+        <div className="recipe__row">
+          <span className="recipe__label">Servings:</span>
+          <span className="recipe__value">{Servings}</span>
+        </div>
+        <div className="recipe__row">
+          <span className="recipe__label">Instructions:</span>
+          <div className="recipe__value recipe__instructions recipe__value--indented">{instructions}</div>
+        </div>
+        <div className="recipe__row">
+          <div className="recipe__label">Ingredients:</div>
+          <div className="recipe__value recipe__value--indented">
+            <IngredientList ingredients={ingredients} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
